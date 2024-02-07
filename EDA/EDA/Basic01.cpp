da85a8586a1ad995daf1efc0c831b509634fd531
jameshuang0430 <yi.11026306@gmail.com>
2024-02-08 01:06:31 +0800

    updated

---
+++ EDA/EDA/Basic01.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int input;
+int sum = 0;
+bool isPrime(int number);
+
+int main(void)
+{
+	cout << "Please enter a positive number." << endl;
+	cin >> input;
+
+	if (input > 0)
+	{
+		for (int i = 2; i <= input; i++)
+		{
+			if (isPrime(i))
+			{
+				sum = sum + i;
+			}
+		}
+		cout << "The sum is: " << sum << endl;
+	}
+	else
+	{
+		cout << "It is not a positive number." << endl;
+	}
+	
+}
+
+bool isPrime(int number)
+{
+	for (int k=2; k < number; k++)
+	{
+		if (number % k == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
